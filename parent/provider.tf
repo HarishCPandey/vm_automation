9e9b1919ed2584a8a73d2067916c3c5517d1e1ad
Harish Pandey <149171752+HarishCPandey@users.noreply.github.com>
2025-08-23 17:22:54 +0530

    Update provider.tf

--- parent/provider.tf
+++ parent/provider.tf
@@ -5,9 +5,16 @@ terraform {
             version = "4.33.0"
         }
     }
+backend "azurerm" {
+    resource_group_name = "automate-todoapp-using-pipeline"
+    storage_account_name = "mystg5577"
+    container_name = "myxontainer5577"
+    key = "mykey5577"
+}
 }
 provider "azurerm" {
     features {}
     subscription_id = "b02b567a-e4cb-4c73-975e-4664c82c8fc3"
 
 } 
+
